b9b2283ce25d9ad57cfb1ee648da5620ce53f71e
ContaIFRN <rodrigues.morais@escolar.ifrn.edu.br>
2026-04-09 10:24:17 -0300

    eh o tal do fox legend
    ksksks vamo começar

---
+++ eh o projeto com Mageski/Assets/Script/Testes 1.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Testes1 : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Quem eh esse neguinho?");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
