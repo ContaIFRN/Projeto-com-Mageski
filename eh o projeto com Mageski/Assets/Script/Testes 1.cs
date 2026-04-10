e16cffae5fcca53abdc1422843691fb6ca2165f5
ContaIFRN <rodrigues.morais@escolar.ifrn.edu.br>
2026-04-10 11:42:59 -0300

    aprendono variaveis
    mageski eh incrivel

--- eh o projeto com Mageski/Assets/Script/Testes 1.cs
+++ eh o projeto com Mageski/Assets/Script/Testes 1.cs
@@ -2,10 +2,22 @@ using UnityEngine;
 
 public class Testes1 : MonoBehaviour
 {
+
+    private int espaçoDeInventario = 10;
+    private bool vivo = true;
+    private [SerializeField] float DanoDoPersonagem = 10.5f;
+    private [SerializeField] float VidaDoPersonagem = 150f;
+    private [SerializeField] string nomeDoPersonagem = "Guerreiro";
+    public string ArmaDoPersonagem = "NomeDaArma";
+    public bool UsaArmadura = true;
+    public float ReduçãoDeDanoDaArmadura = 1.5f;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log("Quem eh esse neguinho?");
+        float danoDoSlime = 12.5f;
+        float vidaDoSlime = 100f;
     }
 
     // Update is called once per frame
