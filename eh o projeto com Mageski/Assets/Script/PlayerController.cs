d98766b2e9475c47eb78c63413550155b6751d20
ContaIFRN <rodrigues.morais@escolar.ifrn.edu.br>
2026-05-27 11:46:31 -0300

    aprendeno a andar
    o bebe ta cresseno

--- eh o projeto com Mageski/Assets/Script/PlayerController.cs
+++ eh o projeto com Mageski/Assets/Script/PlayerController.cs
@@ -3,12 +3,13 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-    [SerializeField] private float movementSpeed;
-    
     private Rigidbody rb;
     private CharacterController controller;
+    private Vector3 direction;
 
 
+    [Header("Player Settings")]
+    [SerializeField] private float movementSpeed;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -20,6 +21,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float  horizontal = Input.GetAxis("Horizontal"); //Estou pegoando o input em x // o "." é para acessar algo dentro de uma classe, nesse caso o "Horizontal" dentro do "Input"
+        float vertical = Input.GetAxis("Verrtical"); //Estou pegoando o input em y  
+
+       direction = new Vector3(horizontal, 0f, vertical).normalized; //o new Vector3 é para criar um vetor, nesse caso o "direction" que tem os valores de "horizontal" e "vertical".
+       //normalized é para normalizar o vetor, ou seja, deixar ele com o mesmo tamanho, independente da direção que ele esteja apontando. 
+       
+
+
     }
 }
