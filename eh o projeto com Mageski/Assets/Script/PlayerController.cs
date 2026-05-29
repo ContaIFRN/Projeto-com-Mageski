aa7f6e34fb1b21963e6a133dd85533ac87f47c14
ContaIFRN <rodrigues.morais@escolar.ifrn.edu.br>
2026-05-29 11:49:03 -0300

    desafio dificil ksksk
    ;-;

--- eh o projeto com Mageski/Assets/Script/PlayerController.cs
+++ eh o projeto com Mageski/Assets/Script/PlayerController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
@@ -22,11 +23,17 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         float  horizontal = Input.GetAxis("Horizontal"); //Estou pegoando o input em x // o "." é para acessar algo dentro de uma classe, nesse caso o "Horizontal" dentro do "Input"
-        float vertical = Input.GetAxis("Verrtical"); //Estou pegoando o input em y  
+        float vertical = Input.GetAxis("Vertical"); //Estou pegoando o input em y  
 
-       direction = new Vector3(horizontal, 0f, vertical).normalized; //o new Vector3 é para criar um vetor, nesse caso o "direction" que tem os valores de "horizontal" e "vertical".
-       //normalized é para normalizar o vetor, ou seja, deixar ele com o mesmo tamanho, independente da direção que ele esteja apontando. 
-       
+        direction = new Vector3(horizontal, 0f, vertical).normalized; //o new Vector3 é para criar um vetor, nesse caso o "direction" que tem os valores de "horizontal" e "vertical".
+        //normalized é para normalizar o vetor, ou seja, deixar ele com o mesmo tamanho, independente da direção que ele esteja apontando. 
+        controller.Move(direction * movementSpeed * Time.deltaTime);
+        
+        if(Direction)
+
+     
+        
+        
 
 
     }
