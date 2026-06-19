4c9da9f59266a9b3e1a50a71d049812623acfeb0
ContaIFRN <rodrigues.morais@escolar.ifrn.edu.br>
2026-06-19 11:49:22 -0300

    tentando atacar
    so q n foi

--- eh o projeto com Mageski/Assets/Script/PlayerController.cs
+++ eh o projeto com Mageski/Assets/Script/PlayerController.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -14,6 +15,7 @@ public class PlayerController : MonoBehaviour
     private Animator animator;
     private bool iWalk;
 
+
     [Header("Player Settings")]
     [SerializeField] private float movementSpeed;
 
@@ -28,31 +30,33 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float  horizontal = Input.GetAxis("Horizontal"); //Estou pegoando o input em x // o "." é para acessar algo dentro de uma classe, nesse caso o "Horizontal" dentro do "Input"
+        float horizontal = Input.GetAxis("Horizontal"); //Estou pegoando o input em x // o "." é para acessar algo dentro de uma classe, nesse caso o "Horizontal" dentro do "Input"
         float vertical = Input.GetAxis("Vertical"); //Estou pegoando o input em y  
         //GetAxis é um metodo do Input que serve para pegar o input do jogador, ele recebe uma string que representa o nome do eixo que queremos pegar, nesse caso "Horizontal" e "Vertical".
 
         direction = new Vector3(horizontal, 0f, vertical).normalized; //o new Vector3 é para criar um vetor, nesse caso o "direction" que tem os valores de "horizontal" e "vertical".
-        //normalized é para normalizar o vetor, ou seja, deixar ele com o mesmo tamanho, independente da direção que ele esteja apontando. 
+                                                                      //normalized é para normalizar o vetor, ou seja, deixar ele com o mesmo tamanho, independente da direção que ele esteja apontando. 
 
-        
+
 
         if (direction.magnitude > 0.1f) //0.1f para identificar mais facilmente se o jogador quer ou não se mover. Se passar de 0.1f, o jogador quer se mover, se for menor, ele não quer se mover.
         {
-           float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg; //Mathf.Atan2 eh uma biblioteca matematica que serve para fazer calculos matematicos
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg; //Mathf.Atan2 eh uma biblioteca matematica que serve para fazer calculos matematicos
             transform.rotation = Quaternion.Euler(0f, targetAngle, 0f); //targetAngle no y pois é a rotação do jogador em torno do eixo y e somente nele.
-            
-            iWalk = true;            
+
+            iWalk = true;
+
         }
         else
         {
-            iWalk = false;  
+            iWalk = false;
         }
 
-        animator.SetBool("iWalk", iWalk); //SetBool é um método do Animator que serve para definir o valor de um parâmetro do Animator, nesse caso o "iWalk" que é um parâmetro do tipo bool.
-        controller.Move(direction * movementSpeed * Time.deltaTime); //Move é um método do CharacterController que serve para mover o personagem, ele recebe um vetor de direção,
-                                                                     //a velocidade e o tempo entre os frames (Time.deltaTime) para garantir que o movimento seja suave e consistente,
-                                                                     //independente da taxa de quadros do jogo.
+            controller.Move(direction * movementSpeed * Time.deltaTime);//a velocidade e o tempo entre os frames (Time.deltaTime) para garantir que o movimento seja suave e consistente,
+                                                                        //independente da taxa de quadros do jogo.  
+
+            animator.SetBool("iWalk", iWalk); //SetBool é um método do Animator que serve para definir o valor de uma variável booleana,
+                                              //nesse caso "isWalking" que é a variável que controla a animação de caminhada do jogador.
 
 
 
@@ -65,5 +69,7 @@ public class PlayerController : MonoBehaviour
 
 
 
+
+        
     }
 }
