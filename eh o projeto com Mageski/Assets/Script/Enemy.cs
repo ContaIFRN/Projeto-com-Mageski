3ce42c87a0b9ab8d964864ec329c7bcbceff9313
ContaIFRN <rodrigues.morais@escolar.ifrn.edu.br>
2026-08-26 08:18:17 -0300

    n sei fazer inimigo com ponto de patrulha ;-;
    vo chora

---
+++ eh o projeto com Mageski/Assets/Script/Enemy.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private float speed;
+    private float visionRange;
+
+    private Rigidbody rb;
+    
+    
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        public Transform patrolPoint;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
