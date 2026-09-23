2badaccadbaf3569e7b07513fe39d4bc7928b186
ContaIFRN <rodrigues.morais@escolar.ifrn.edu.br>
2026-09-23 08:19:10 -0300

    n sei faze vida ;-;

---
+++ eh o projeto com Mageski/Assets/Script/POO/CabaDoDano.cs
@@ -0,0 +1,26 @@
+using System;
+using UnityEngine;
+
+public class CabaDoDano : Persona
+{
+    private int Dano;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        nome = "CabaDoDano";
+        vida = 23;
+        velocidade = 3.0f;
+        manaMax = 14;
+        força = 14;
+        tec = 5;
+
+        ;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ eh o projeto com Mageski/Assets/Script/POO/InimgoAe.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class InimgoAe : SistemaDeVIda
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        vidaMax = 20;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
